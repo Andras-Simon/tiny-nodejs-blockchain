5cbc8dcd4af2715c571b045c47e6d7ef6c88ab10
Andras <andras@cabsolutely.co>
2020-05-06 16:54:38 +0200

    Create block.js

---
+++ src/block.js
@@ -0,0 +1,27 @@
+const SHA256 = require("crypto-js/sha256");
+
+class Block {
+    constructor(index, nonce, timestamp, data, lastHash = " ", targetDifficulty = 5) {
+        this.index = index;
+        this.nonce = nonce;
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.data = data;
+        this.targetDifficulty = targetDifficulty;
+    }
+
+    getDataHash() {
+        return SHA256(JSON.stringify(this.data));
+    }
+
+    getHash() {
+        return SHA256(
+            this.index +
+            this.nonce +
+            this.lastHash +
+            this.timestamp +
+            this.targetDifficulty +
+            this.getDataHash()
+        );
+    }
+}
